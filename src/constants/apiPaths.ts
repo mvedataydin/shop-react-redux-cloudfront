f406102bcbade72d0f8c1d20cb068925f8989323
mvedataydin <mvedataydin@gmail.com>
2022-08-02 14:57:12 +0300

    feat: implement csv file import service

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://v8cwkhenx0.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://vz94pc6b2l.execute-api.eu-west-1.amazonaws.com/dev",
   bff: "https://v8cwkhenx0.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
